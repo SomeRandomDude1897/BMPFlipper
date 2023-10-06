316276936d1661dc771d175ab508d720a10f7551
SomeRandomSpbuStudent <sokhanivan@yandex.ru>
2023-10-06 16:24:45 +0300

    tried to clean memory

--- BMPFlipper/BMPFlipper.cpp
+++ BMPFlipper/BMPFlipper.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 
 #pragma warning(disable:4996)
-#define min(a,b) ((a)<(b)?(a):(b))
 
 using namespace std;
 
@@ -21,27 +20,32 @@ struct header_data
     uint32_t colornum{ 0 };
     uint32_t impcolors{ 0 };
 };
-struct pixel {
-    uint32_t b;
-    uint32_t g;
-    uint32_t r;
+/*
+struct file_data
+{
+    uint16_t id;
+    uint32_t file_size;
+    uint16_t reserved1;
+    uint16_t reserved2;
+    uint32_t padding
 };
+*/
 int main()
 {
-    FILE* file = fopen("sea.bmp", "rb");
+    FILE* file = fopen("field.bmp", "rb");
 
-    header_data headdata;
-    unsigned char info[14];
+    header_data head_data;
+    unsigned char info[14]; // Я не понимаю почему, но по какой-то причине у меня не считывается через структуру размер файла, поэтому единственное, что я придумал это считать данные всырую
 
     fread(info, 1, 14, file);
-    fread((char*)&headdata, 1, 40, file);
-    cout << "File size: " << *(int*)&info[2] / 1024 << " bytes" << endl;
+    fread((char*)&head_data, 1, 40, file);
+    cout << "File size: " << *(int*)&info[2] / 1024 << " kbytes" << endl;
 
-    unsigned char* extra_header_data = new unsigned char[headdata.dib - 40];
+    unsigned char* extra_header_data = new unsigned char[head_data.dib - 40];
 
-    fread(extra_header_data, 1, headdata.dib - 40, file);
+    fread(extra_header_data, 1, head_data.dib - 40, file);
 
-    int size = headdata.width * headdata.height * 3;
+    int size = head_data.width * head_data.height * 3;
 
     unsigned char* data = new unsigned char[size];
 
@@ -49,150 +53,163 @@ int main()
 
     fclose(file);
 
-    header_data headdata2 = headdata;
-    headdata2.width = headdata.height;
-    headdata2.height = headdata.width;
+    header_data head_data2 = head_data;
+    head_data2.width = head_data.height;
+    head_data2.height = head_data.width;
 
     FILE* outfile1 = fopen("flipped right.bmp", "wb");
     fwrite((const char*)&info, 1, 14, outfile1);
-    fwrite((const char*)&headdata2, 1, 40, outfile1);
-    fwrite(extra_header_data, 1, headdata.dib - 40, outfile1);
-    int extraheight = headdata.height % 4;
-    int extrawidth = headdata.width % 4;
+    fwrite((const char*)&head_data2, 1, 40, outfile1);
+    fwrite(extra_header_data, 1, head_data.dib - 40, outfile1);
+    int extraheight = head_data.height % 4;
+    int extrawidth = head_data.width % 4;
 
-    for (int y = 0; y < headdata.width; y++)
+    int b = 0;
+    int g = 0;
+    int r = 0;
+
+    unsigned char* towriteline = new unsigned char[head_data.height * 3 + extraheight];
+
+    for (int y = 0; y < head_data.width; y++)
     {
-        unsigned char* towriteline = new unsigned char[headdata.height * 3 + extraheight];
-        for (int x = 0; x < headdata.height; x++)
+        
+        for (int x = 0; x < head_data.height; x++)
         {
-            int b = data[(x * headdata.width + (headdata.width - y - 1)) * 3];
-            int g = data[(x * headdata.width + (headdata.width - y - 1)) * 3 + 1];
-            int r = data[(x * headdata.width + (headdata.width - y - 1)) * 3 + 2];
+            b = data[(x * head_data.width + (head_data.width - y - 1)) * 3];
+            g = data[(x * head_data.width + (head_data.width - y - 1)) * 3 + 1];
+            r = data[(x * head_data.width + (head_data.width - y - 1)) * 3 + 2];
             towriteline[x * 3] = b;
             towriteline[x * 3 + 1] = g;
             towriteline[x * 3 + 2] = r;
         }
         for (int i = 0; i < extraheight; ++i)
         {
-            towriteline[headdata.height * 3 + i * 3] = 0;
-            towriteline[headdata.height * 3 + i * 3 + 1] = 0;
-            towriteline[headdata.height * 3 + i * 3 + 2] = 0;
+            towriteline[head_data.height * 3 + i * 3] = 0;
+            towriteline[head_data.height * 3 + i * 3 + 1] = 0;
+            towriteline[head_data.height * 3 + i * 3 + 2] = 0;
         }
-        fwrite(towriteline, 1, headdata.height * 3 + extraheight, outfile1);
+        fwrite(towriteline, 1, head_data.height * 3 + extraheight, outfile1);
+        
     }
+    
     for (int i = 0; i < extrawidth; ++i)
     {
-        unsigned char* towriteline = new unsigned char[headdata.height * 3 + extraheight];
-        for (int x = 0; x < headdata.height + extraheight; x++)
+        for (int x = 0; x < head_data.height + extraheight; x++)
         {
             towriteline[x * 3] = 0;
             towriteline[x * 3 + 1] = 0;
             towriteline[x * 3 + 2] = 0;
         }
-        fwrite(towriteline, 1, headdata.height * 3 + extraheight, outfile1);
+        fwrite(towriteline, 1, head_data.height * 3 + extraheight, outfile1);
     }
 
     fclose(outfile1);
 
     FILE* outfile2 = fopen("flipped left.bmp", "wb");
     fwrite((const char*)&info, 1, 14, outfile2);
-    fwrite((const char*)&headdata2, 1, 40, outfile2);
-    fwrite(extra_header_data, 1, headdata.dib - 40, outfile2);
-    for (int y = 0; y < headdata.width; y++)
+    fwrite((const char*)&head_data2, 1, 40, outfile2);
+    fwrite(extra_header_data, 1, head_data.dib - 40, outfile2);
+    for (int y = 0; y < head_data.width; y++)
     {
-        unsigned char* towriteline = new unsigned char[headdata.height * 3 + extraheight];
-        for (int x = headdata.height - 1; x >= 0; x--)
+        for (int x = head_data.height - 1; x >= 0; x--)
         {
-            int b = data[(x * headdata.width + (headdata.width - y - 1)) * 3];
-            int g = data[(x * headdata.width + (headdata.width - y - 1)) * 3 + 1];
-            int r = data[(x * headdata.width + (headdata.width - y - 1)) * 3 + 2];
-            towriteline[(headdata.height - x) * 3] = b;
-            towriteline[(headdata.height - x) * 3 + 1] = g;
-            towriteline[(headdata.height - x) * 3 + 2] = r;
+            b = data[(x * head_data.width + (head_data.width - y - 1)) * 3];
+            g = data[(x * head_data.width + (head_data.width - y - 1)) * 3 + 1];
+            r = data[(x * head_data.width + (head_data.width - y - 1)) * 3 + 2];
+            towriteline[(head_data.height - x) * 3] = b;
+            towriteline[(head_data.height - x) * 3 + 1] = g;
+            towriteline[(head_data.height - x) * 3 + 2] = r;
         }
         for (int i = 0; i < extraheight; ++i)
         {
-            towriteline[headdata.height * 3 + i * 3] = 0;
-            towriteline[headdata.height * 3 + i * 3 + 1] = 0;
-            towriteline[headdata.height * 3 + i * 3 + 2] = 0;
+            towriteline[head_data.height * 3 + i * 3] = 0;
+            towriteline[head_data.height * 3 + i * 3 + 1] = 0;
+            towriteline[head_data.height * 3 + i * 3 + 2] = 0;
         }
-        fwrite(towriteline, 1, headdata.height * 3 + extraheight, outfile2);
+
+        fwrite(towriteline, 1, head_data.height * 3 + extraheight, outfile2);
     }
     for (int i = 0; i < extrawidth; ++i)
     {
-        unsigned char* towriteline = new unsigned char[headdata.height * 3 + extraheight];
-        for (int x = 0; x < headdata.height + extraheight; x++)
+        for (int x = 0; x < head_data.height + extraheight; x++)
         {
             towriteline[x * 3] = 0;
             towriteline[x * 3 + 1] = 0;
             towriteline[x * 3 + 2] = 0;
         }
-        fwrite(towriteline, 1, headdata.height * 3 + extraheight, outfile2);
     }
+
+    fclose(outfile2);
+
+
     double pi = 3.14;
-    int r = 2;
+    int radius = 5;
     double sigma = 3;
     vector<vector<double>> gauss;
     double summ = 0;
-    for (int h = 0; h < 2 * r; h++) {
+    for (int h = 0; h < 2 * radius; h++) {
         gauss.push_back(vector<double>());
-        for (int w = 0; w < 2 * r; w++) {
-            gauss[h].push_back(exp(-((h - r) * (h - r) + (w - r) * (w - r)) / (2 * (sigma * sigma))) / (2 * pi * sigma * sigma));
+        for (int w = 0; w < 2 * radius; w++) {
+            gauss[h].push_back(exp(-((h - radius) * (h - radius) + (w - radius) * (w - radius)) / (2 * (sigma * sigma))) / (2 * pi * sigma * sigma));
             summ += gauss[h][w];
         }
     }
     FILE* outfile3 = fopen("with gaussian filter.bmp", "wb");
     fwrite((const char*)&info, 1, 14, outfile3);
-    fwrite((const char*)&headdata, 1, 40, outfile3);
-    fwrite(extra_header_data, 1, headdata.dib - 40, outfile3);
-    for (int y = 0; y < headdata.height; y++)
+    fwrite((const char*)&head_data, 1, 40, outfile3);
+    fwrite(extra_header_data, 1, head_data.dib - 40, outfile3);
+    unsigned char* towriteline2 = new unsigned char[head_data.width * 3 + extrawidth];
+    for (int y = 0; y < head_data.height; y++)
     {
-        unsigned char* towriteline = new unsigned char[headdata.width * 3 + extrawidth];
-        for (int x = 0; x < headdata.width; x++)
+        
+        for (int x = 0; x < head_data.width; x++)
         {
             double gaussumb = 0;
             double gaussumg = 0;
             double gaussumr = 0;
-            for (int h = 0; h < 2 * r; h++) {
-                for (int w = 0; w < 2 * r; w++) {
-                    if (y + h - r >= 0 && x + w - r >= 0 && y + h - r < headdata.height && x + w - r < headdata.width)
+            for (int h = 0; h < 2 * radius; h++) {
+                for (int w = 0; w < 2 * radius; w++) {
+                    if (y + h - radius >= 0 && x + w - radius >= 0 && y + h - radius < head_data.height && x + w - radius < head_data.width)
                     {
-                        gaussumb += gauss[h][w] / summ * data[((y + h - r) * headdata.width + x - r + w) * 3];
-                        gaussumg += gauss[h][w] / summ * data[((y + h - r) * headdata.width + x - r + w) * 3 + 1];
-                        gaussumr += gauss[h][w] / summ * data[((y + h - r) * headdata.width + x - r + w) * 3 + 2];
+                        gaussumb += gauss[h][w] / summ * data[((y + h - radius) * head_data.width + x - radius + w) * 3];
+                        gaussumg += gauss[h][w] / summ * data[((y + h - radius) * head_data.width + x - radius + w) * 3 + 1];
+                        gaussumr += gauss[h][w] / summ * data[((y + h - radius) * head_data.width + x - radius + w) * 3 + 2];
                     }
                 }
             }
-            int b = gaussumb;
-            int g = gaussumg;
-            int r = gaussumr;
-            data[((y)*headdata.width + x) * 3] = b;
-            data[((y)*headdata.width + x) * 3 + 1] = g;
-            data[((y)*headdata.width + x) * 3 + 2] = r;
-            towriteline[x * 3] = b;
-            towriteline[x * 3 + 1] = g;
-            towriteline[x * 3 + 2] = r;
+            b = gaussumb;
+            g = gaussumg;
+            r = gaussumr;
+            data[((y)*head_data.width + x) * 3] = b;
+            data[((y)*head_data.width + x) * 3 + 1] = g;
+            data[((y)*head_data.width + x) * 3 + 2] = r;
+            towriteline2[x * 3] = b;
+            towriteline2[x * 3 + 1] = g;
+            towriteline2[x * 3 + 2] = r;
         }
         for (int i = 0; i < extrawidth; ++i)
         {
-            towriteline[headdata.width * 3 + i * 3] = 0;
-            towriteline[headdata.width * 3 + i * 3 + 1] = 0;
-            towriteline[headdata.width * 3 + i * 3 + 2] = 0;
+            towriteline2[head_data.width * 3 + i * 3] = 0;
+            towriteline2[head_data.width * 3 + i * 3 + 1] = 0;
+            towriteline2[head_data.width * 3 + i * 3 + 2] = 0;
         }
-        fwrite(towriteline, 1, headdata.width * 3 + extrawidth, outfile3);
+        fwrite(towriteline2, 1, head_data.width * 3 + extrawidth, outfile3);
     }
     for (int i = 0; i < extraheight; ++i)
     {
-        unsigned char* towriteline = new unsigned char[headdata.height * 3 + extraheight];
-        for (int x = 0; x < headdata.width + extraheight; x++)
+        for (int x = 0; x < head_data.width + extraheight; x++)
         {
-            towriteline[x * 3] = 0;
-            towriteline[x * 3 + 1] = 0;
-            towriteline[x * 3 + 2] = 0;
+            towriteline2[x * 3] = 0;
+            towriteline2[x * 3 + 1] = 0;
+            towriteline2[x * 3 + 2] = 0;
         }
-        fwrite(towriteline, 1, headdata.width * 3 + extraheight, outfile3);
+        fwrite(towriteline2, 1, head_data.width * 3 + extraheight, outfile3);
     }
-    fclose(outfile2);
+
+
+    delete[] data;
+
+    return 0;
 
 
 }
